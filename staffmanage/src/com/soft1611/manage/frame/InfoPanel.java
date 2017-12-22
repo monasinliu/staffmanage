9964860598ef03e1cc8ef86818b980c24cac3eeb
zhuguangxu <1791219406@qq.com>
2017-12-22 22:38:42 +0800

    查看考勤+个人信息
    查看考勤界面，功能还未全部实现，界面底部面板还未添加功能按钮。

--- staffmanage/src/com/soft1611/manage/frame/InfoPanel.java
+++ staffmanage/src/com/soft1611/manage/frame/InfoPanel.java
@@ -8,6 +8,8 @@ import com.soft1611.manage.service.StaffService;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 /**
@@ -37,12 +39,13 @@ public class InfoPanel extends JPanel{
     private JTextField phoneField;
     private JTextField emailField;
     private JButton jButton1;
-    private String[] info = {"基本资料","工   号：","部   门: ","姓   名: "
-            ,"性   别：","民   族：","籍   贯：","住   址：","学   历：",
-            "职   务：","职   称：","政治面貌：","入职时间：","联系方式：",
+    private String[] info = {"基本资料","工     号：","部     门: ","姓     名: "
+            ,"性     别：","民     族：","籍     贯：","住     址：","学     历：",
+            "职     务：","职     称：","政治面貌：","入职时间：","联系方式：",
             "电子邮箱："};
-    Font font  = new Font("微软雅黑",Font.PLAIN,18);
-    Font font1 = new Font("微软雅黑",Font.PLAIN,14);
+    Font font  = new Font("微软雅黑",Font.BOLD,18);
+    Font font1 = new Font("微软雅黑",Font.BOLD,14);
+    Font font2  = new Font("微软雅黑",Font.BOLD,16);
     Color color = new Color(51,102,153);
     private StaffService staffService = ServiceFactory.getStaffService();
     public InfoPanel(Staff staff) {
@@ -50,8 +53,8 @@ public class InfoPanel extends JPanel{
         labels = new JLabel[15];
         for(int i = 0; i < labels.length ; i++){
             labels[i] = new JLabel(info[i]);
-            labels[i].setPreferredSize(new Dimension(100,30));
-            labels[i].setFont(font1);
+            labels[i].setPreferredSize(new Dimension(80,30));
+            labels[i].setFont(font2);
         }
         bigPanel = new JPanel();
         bigPanel.setBackground(Color.white);
@@ -69,6 +72,7 @@ public class InfoPanel extends JPanel{
             panels[i] = new JPanel();
             panels[i].setLayout(new FlowLayout(FlowLayout.LEFT, 40, 0));
             panels[i].setBackground(Color.WHITE);
+
         }
         panels[0].add(labels[0]);
         labels[0].setFont(font);
@@ -76,81 +80,112 @@ public class InfoPanel extends JPanel{
         //第一行面板
         panels[1].add(labels[1]);
         accountField = new JTextField();
+        accountField.setFont(font1);
         accountField.setPreferredSize(new Dimension(200, 30));
         panels[1].add(accountField);
         //第二行面板
         panels[2].add(labels[2]);
         departmentField = new JTextField();
+        departmentField.setFont(font1);
         departmentField.setPreferredSize(new Dimension(200, 30));
         panels[2].add(departmentField);
         //3
         panels[3].add(labels[3]);
         nameField = new JTextField();
+        nameField.setFont(font1);
         nameField.setPreferredSize(new Dimension(200, 30));
         panels[3].add(nameField);
         //4
         panels[4].add(labels[4]);
         sexField = new JTextField();
+        sexField.setFont(font1);
         sexField.setPreferredSize(new Dimension(200, 30));
         panels[4].add(sexField);
         //5
         panels[5].add(labels[5]);
         nationField = new JTextField();
+        nationField.setFont(font1);
+        //去掉文本框外框
+        //accountField.setBorder(BorderFactory.createLineBorder(Color.white));
         nationField.setPreferredSize(new Dimension(200, 30));
         panels[5].add(nationField);
         //6
         panels[6].add(labels[6]);
         proviceField = new JTextField();
+        proviceField.setFont(font1);
         proviceField.setPreferredSize(new Dimension(200, 30));
         panels[6].add(proviceField);
         //7
         panels[7].add(labels[7]);
         addressField = new JTextField();
+        addressField.setFont(font1);
         addressField.setPreferredSize(new Dimension(200, 30));
         panels[7].add(addressField);
-
-
-
         //8
         panels[8].add(labels[8]);
         educationField = new JTextField();
+        educationField.setFont(font1);
         educationField.setPreferredSize(new Dimension(200, 30));
         panels[8].add(educationField);
         //9
         panels[9].add(labels[9]);
         dutyField = new JTextField();
+        dutyField.setFont(font1);
         dutyField.setPreferredSize(new Dimension(200, 30));
         panels[9].add(dutyField);
         //10
         panels[10].add(labels[10]);
         titleField = new JTextField();
+        titleField.setFont(font1);
         titleField.setPreferredSize(new Dimension(200, 30));
         panels[10].add(titleField);
         //11
         panels[11].add(labels[11]);
         political_statusField = new JTextField();
+        political_statusField.setFont(font1);
         political_statusField.setPreferredSize(new Dimension(200, 30));
         panels[11].add(political_statusField);
         //12
         panels[12].add(labels[12]);
         sign_in_timeField = new JTextField();
+        sign_in_timeField.setFont(font1);
         sign_in_timeField.setPreferredSize(new Dimension(200, 30));
         panels[12].add(sign_in_timeField);
         //13
         panels[13].add(labels[13]);
         phoneField = new JTextField();
+        phoneField.setFont(font1);
         phoneField.setPreferredSize(new Dimension(200, 30));
         panels[13].add(phoneField);
         //14
         panels[14].add(labels[14]);
         emailField = new JTextField();
+        emailField.setFont(font1);
         emailField.setPreferredSize(new Dimension(200, 30));
         panels[14].add(emailField);
         //15
         jButton1 = new JButton("编   辑");
-        jButton1.setForeground(color);
+        jButton1.setFont(font2);
+        jButton1.setBackground(color);
+
         panels[15].add(jButton1);
+
+        //获取信息显示
         accountField.setText(staff.getAccount());
+        departmentField.setText(staff.getDepartment());
+        nameField.setText(staff.getName());
+        sexField.setText(staff.getSex());
+        nationField.setText(staff.getNation());
+        proviceField.setText(staff.getProvice());
+        addressField.setText(staff.getAddress());
+        educationField.setText(staff.getEducation());
+        dutyField.setText(staff.getDuty());
+        titleField.setText(staff.getTitle());
+        political_statusField.setText(staff.getPolitical_status());
+        sign_in_timeField.setText(String.valueOf(staff.getSign_in_time()));
+        phoneField.setText(staff.getPhone());
+        emailField.setText(staff.getEmail());
+
         for (int i = 0; i < panels.length; i++) {
             bigPanel.add(panels[i]);
         }
@@ -175,4 +210,5 @@ public class InfoPanel extends JPanel{
         frame.setVisible(true);
 
     }
+
 }
--- staffmanage/src/com/soft1611/manage/frame/InfoPanel2.java
+++ staffmanage/src/com/soft1611/manage/frame/InfoPanel2.java
@@ -1,7 +1,136 @@
 package com.soft1611.manage.frame;
 
+import com.soft1611.manage.dao.AttendanceDAO;
+import com.soft1611.manage.dao.StaffDAO;
+import com.soft1611.manage.factory.DAOFactory;
+import com.soft1611.manage.factory.ServiceFactory;
+import com.soft1611.manage.model.Attendance;
+import com.soft1611.manage.model.Staff;
+import com.soft1611.manage.service.StaffService;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.Iterator;
+
 /**
  * Created by 朱广旭 on 2017/12/21.
  */
-public class InfoPanel2 {
+public class InfoPanel2 extends JPanel{
+    private JTable table;
+    private DefaultTableModel dtm;
+    private DefaultTableCellRenderer renderer,renderer1;
+    protected java.util.List<Attendance> attendances;
+    Iterator<Attendance> iterator = null;
+    private AttendanceDAO attendanceDAO = DAOFactory.getAttendanceDAOInstance();
+    private Staff staff;
+    private StaffService staffService = ServiceFactory.getStaffService();
+    private InfoPanel2 infoPanel2;
+    private JPanel topPanel;
+    private JPanel centerPanel;
+    private JPanel bottonPanel;
+    private JLabel titleLabel;
+    private JButton[] jButtons;
+    private String[] info = {"上一页","下一页","导出数据","打   印"};
+    Font font  = new Font("微软雅黑",Font.PLAIN,22);
+    Font font1 = new Font("微软雅黑",Font.PLAIN,14);
+    Font font2  = new Font("微软雅黑",Font.BOLD,16);
+    Color color = new Color(66, 139, 202);
+
+    public InfoPanel2(Staff staff){
+        this.staff=staff;
+        setLayout(new BorderLayout());
+        //setBackground(Color.WHITE);
+        setSize(800,800);
+        topPanel=new JPanel();
+        topPanel.setBackground(Color.white);
+        centerPanel = new JPanel();
+        centerPanel.setBackground(Color.white);
+        bottonPanel = new JPanel();
+        bottonPanel.setBackground(Color.BLUE);
+        add(getTopPanel(),BorderLayout.NORTH);
+        add(getCenterPanel(),BorderLayout.CENTER);
+        add(getBottonPanel(),BorderLayout.SOUTH);
+    }
+
+    public JPanel getTopPanel(){
+        titleLabel = new JLabel("个人出勤记录");
+        titleLabel.setBackground(Color.BLACK);
+        titleLabel.setFont(font);
+        setLayout(new BorderLayout());
+        topPanel.setPreferredSize(new Dimension(800,50));
+        topPanel.add(titleLabel,BorderLayout.SOUTH);
+        return topPanel;
+    }
+    public JPanel getCenterPanel() {
+       centerPanel.setLayout(new FlowLayout());
+       table = new JTable();
+       table.setPreferredSize(new Dimension(700,800));
+       table.setFont(font1);
+       renderer = new DefaultTableCellRenderer();
+       renderer1 = new DefaultTableCellRenderer();
+       dtm = new DefaultTableModel();
+       String[] titles = {"日   期","姓   名","工   号","部   门","出勤记录"};
+       dtm.setColumnIdentifiers(titles);
+       table.setModel(dtm);
+       renderer.setHorizontalAlignment(JLabel.CENTER);
+       renderer.setFont(font1);
+       table.setDefaultRenderer(Object.class,renderer);
+       renderer1.setHorizontalAlignment(JLabel.CENTER);
+       renderer1.setBackground(color);
+       renderer1.setForeground(Color.white);
+       renderer.setBackground(Color.white);
+       table.getTableHeader().setDefaultRenderer(renderer1);
+       String[] content = new String[5];
+       attendances = staffService.getAttendance(staff.getAccount());
+       iterator = attendances.iterator();
+       while(iterator.hasNext()){
+          Attendance attendance = iterator.next();
+            content[0] = String.valueOf(attendance.getAttend_time());
+            content[1] = staff.getName();
+            content[2] = attendance.getAccount();
+            content[3] = staff.getDepartment();
+            content[4] = attendance.getAttend_case();
+//            if(content[0] != null){
+//                content[4] = "已出勤";
+//           }else{ 
+//                content[4] = null;
+//            }
+            dtm.addRow(content);
+       }
+       JScrollPane scrollPane=new JScrollPane();
+       scrollPane.setPreferredSize(new Dimension(760,620));
+       scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+       scrollPane.setViewportView(table);
+       centerPanel.add(scrollPane);
+        return centerPanel;
+    }
+    public JPanel getBottonPanel() {
+
+        return bottonPanel;
+    }
+    public static void main(String[] args) throws SQLException {
+        StaffDAO staffDAO = DAOFactory.getStaffDAOInstance();
+        try {
+            System.out.println(staffDAO.get("20010101").getAccount());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("测试窗体");
+        frame.setSize(800, 800);
+        frame.setLocationRelativeTo(null);
+//        frame.add(new InfoPanel2());
+//        try {
+            frame.add(new InfoPanel2(staffDAO.get("20010101")));
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+        frame.setVisible(true);
+
+    }
+
+
+
 }
