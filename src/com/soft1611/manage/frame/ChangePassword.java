a5e704f2de24a0008eee144fa646d5e4820e13cf
monasinliu <31915974+monasinliu@users.noreply.github.com>
2017-12-26 10:59:43 +0800

    12/26
    change password

--- src/com/soft1611/manage/frame/ChangePassword.java
+++ src/com/soft1611/manage/frame/ChangePassword.java
@@ -16,36 +16,55 @@ import java.util.Map;
  */
 public class ChangePassword extends JPanel {
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
     private JButton 确认保存Button;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JPasswordField passwordField3;
     private User user;
 
     private UserService userService = ServiceFactory.getUserServiceInstance();
 
-    public ChangePassword(User user){
-
+    public ChangePassword(User user) {
+        this.user = user;
         add(mainPanel);
-
+        String oldPassword = new String(passwordField1.getPassword());
+        String newPassword = new String(passwordField2.getPassword());
+        String confirmPassword = new String(passwordField3.getPassword());
 
         确认保存Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(textField1.getText().equals(user.getPassword())){
-                    if(textField2.getText().equals(textField1.getText())){
-                        JOptionPane.showConfirmDialog(null,"不能与原密码一致！");
-                    }else if(textField2.getText().equals(textField3.getText())){
-                        if(textField2.getText() == null){
-                            JOptionPane.showConfirmDialog(null,"不能为空！");
-                        }else{
-                            userService.changePassword()
-                            JOptionPane.showConfirmDialog(null,"重置成功！");
+                if (oldPassword.equals(user.getPassword())){
+                    if (newPassword!=null){
+                        if (newPassword.equals(oldPassword)){
+                            JOptionPane.showConfirmDialog(null, "不能与原密码一致！");
+                        }else {
+                            if (confirmPassword.equals(newPassword)){
+                                userService.changePassword(user.getAccount(),newPassword);
+                                JOptionPane.showConfirmDialog(null, "重置成功！");
+                            }else {
+                                JOptionPane.showConfirmDialog(null, "两次输入密码不一致");
+                            }
                         }
+                    }else {
+                        JOptionPane.showConfirmDialog(null, "不能为空！");
                     }
-                }else{
-                    JOptionPane.showConfirmDialog(null,"密码错误！");
+                } else{
+                    JOptionPane.showConfirmDialog(null, "原密码错误！");
                 }
+//                if (oldPassword.equals(user.getPassword())){
+//                    if (newPassword.equals(oldPassword)) {
+//                        JOptionPane.showConfirmDialog(null, "不能与原密码一致！");
+//                    } else if (confirmPassword.equals(newPassword)) {
+//                        if (textField2.getText() == null) {
+//                            JOptionPane.showConfirmDialog(null, "不能为空！");
+//                        } else {
+//
+//                        }
+//                    }
+//                } else{
+//                    JOptionPane.showConfirmDialog(null, "原密码错误！");
+//                }
             }
         });
     }
